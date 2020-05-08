30767d94f3cd70b51c151fd58a1f9a9761448448
rs31-zy <1137111380@qq.com>
2020-05-08 20:27:06 +0800

    城市筛选及联动筛选

--- apps/organizations/views.py
+++ apps/organizations/views.py
@@ -11,20 +11,26 @@ class OrgView(View):
         #展示授课机构
 
         all_orgs = CourseOrg.objects.all()
-        org_nums = CourseOrg.objects.all().count()
         all_citys = City.objects.all()
 
         org = request.GET.get("ct","")
         if org:
             all_orgs = all_orgs.filter(org = org)
 
+        city_id = request.GET.get('city','')
+        if city_id:
+            if city_id.isdigit():
+                all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        org_nums = all_orgs.count()
+
 
         try:
             page = request.GET.get('page',1)
         except PageNotAnInteger:
             page = 1
-        p = Paginator(all_orgs,per_page=2, request=request)
+        p = Paginator(all_orgs,per_page=5, request=request)
         orgs = p.page(page)
 
 
-        return render(request,'org-list.html',{'all_orgs':orgs,'org_nums':org_nums,'all_citys':all_citys,'org':org})
+        return render(request,'org-list.html',{'all_orgs':orgs,'org_nums':org_nums,'all_citys':all_citys,'org':org,'city_id':city_id,})
