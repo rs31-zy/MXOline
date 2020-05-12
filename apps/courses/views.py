4120bded97d33423018b0aa066f2e58eff285e11
rs31-zy <1137111380@qq.com>
2020-05-12 19:57:51 +0800

    首页高亮切换

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -7,14 +7,24 @@ from pure_pagination import Paginator, PageNotAnInteger
 
 class CourseView(View):
     def get(self,request,*args,**kwargs):
-        all_courses = Course.objects.all()
+        all_courses = Course.objects.order_by('-add_time')
+        hot_courses = all_courses.order_by("-student")[:3]
+
+        sort = request.GET.get('sort', '')
+        if sort == 'hot':
+            all_courses = all_courses.order_by('-student')
+        elif sort == 'students':
+            all_courses = all_courses.order_by('click_nums')
 
         try:
             page = request.GET.get('page',1)
         except PageNotAnInteger:
             page = 1
-        p = Paginator(all_courses,per_page=4, request=request)
+        p = Paginator(all_courses,per_page=3, request=request)
         courses = p.page(page)
 
-        return render(request,'course-list.html',{'all_courses':courses})
+        return render(request,'course-list.html',{'all_courses':courses,
+                                                  'sort':sort,
+                                                  'hot_courses':hot_courses
+                                                  })
 
