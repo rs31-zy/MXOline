3846f7a4d61b8d1a804f545af758f1797ead3360
rs31-zy <1137111380@qq.com>
2020-05-27 18:07:25 +0800

    我的收藏

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
-from apps.users.views import UserInfoView
+from apps.users.views import UserInfoView, MyFavOrgView, MyFavTeacherView, MyFavCourseView, ChangePwdView
 
 urlpatterns = [
     url(r'^info/$', UserInfoView.as_view(),name='info'),
+    url(r'^mycourse/$', login_required(TemplateView.as_view(template_name="usercenter-mycourse.html"),login_url='/login/'),{'current_page':'mycourse'}, name='mycourse'),
+    url(r'^myfavorg/$', MyFavOrgView.as_view(),name='myfavorg'),
+    url(r'^myfav_teacher/$', MyFavTeacherView.as_view(), name='myfav_teacher'),
+    url(r'^myfav_course/$', MyFavCourseView.as_view(), name='myfav_course'),
+    url(r'^update/pwd/$', ChangePwdView.as_view(), name='update_pwd/'),
+
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic.base import View
-from django.http import HttpResponseRedirect
-from apps.users.form import LoginForm
+from django.http import HttpResponseRedirect, JsonResponse
+
+from apps.courses.models import Course
+from apps.operations.models import UserFavorite
+from apps.organizations.models import CourseOrg, Teacher
+from apps.users.form import LoginForm, ChangePwdForm
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 
@@ -42,6 +46,66 @@ class LogoutView(View):
 class UserInfoView(LoginRequiredMixin,View):
     login_url = '/login/'
     def get(self,request,*args,**kwargs):
+        current_page = 'info'
+
+        return render(request,'usercenter-info.html',{'current_page':current_page,
+                                                      })
 
 
-        return render(request,'usercenter-info.html')
+class MyFavOrgView(LoginRequiredMixin,View):
+    login_url = '/login/'
+    def get(self,request,*args,**kwargs):
+        current_page = 'myfavorg'
+        fav_orgs = UserFavorite.objects.filter(user=request.user,fav_type=2)
+        org_list = []
+        for fav_org in fav_orgs:
+            org = CourseOrg.objects.get(id=fav_org.fav_id)
+            org_list.append(org)
+        return render(request,'usercenter-fav-org.html',{'current_page':current_page,
+                                                         'org_list':org_list,
+                                                         })
+
+
+class MyFavTeacherView(LoginRequiredMixin, View):
+    login_url = '/login/'
+
+    def get(self,request,*args,**kwargs):
+        current_page = 'myfavorg'
+        fav_teachers = UserFavorite.objects.filter(user=request.user,fav_type=3)
+        teacher_list = []
+        for fav_teacher in fav_teachers:
+            teacher = Teacher.objects.get(id=fav_teacher.fav_id)
+            teacher_list.append(teacher)
+        return render(request,'usercenter-fav-teacher.html',{'current_page':current_page,
+                                                             'teacher_list':teacher_list,
+                                                             })
+
+
+
+class MyFavCourseView(LoginRequiredMixin, View):
+    login_url = '/login/'
+    def get(self,request,*args,**kwargs):
+        current_page = 'myfavorg'
+        fav_courses = UserFavorite.objects.filter(user=request.user, fav_type=1)
+        course_list = []
+        for fav_course in fav_courses:
+            course = Course.objects.get(id=fav_course.fav_id)
+            course_list.append(course)
+        return render(request, 'usercenter-fav-course.html', {'current_page': current_page,
+                                                              'course_list': course_list,
+                                                               })
+
+
+class ChangePwdView(LoginRequiredMixin, View):
+    login_url = '/login/'
+    def post(self,request,*args,**kwargs):
+        pwd_form = ChangePwdForm(request.POST)
+
+        if pwd_form.is_valid():
+            pwd1 = request.POST.get('password1','')
+            user = request.user
+            user.set_password(pwd1)
+            user.save()
+            return JsonResponse({'status':'success'})
+        else:
+            return JsonResponse(pwd_form.errors)
